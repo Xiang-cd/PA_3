78ff881d6773e9e763277f8e38b01ec1fb77c345
xiangxiaoyu <xcd19@mails.tsinghua.edu.cn>
2021-11-29 18:37:01 +0800

    main add

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+#define Debug false
+#define Time false
+clock_t sT;
+#define NN cout<<endl;
+#define PD(x) printf("%d ",(x));
+#define Tik if(Time)sT=clock();
+#define Tok if(Time)cout<<(double)(clock() - sT)/CLOCKS_PER_SEC<<"s"<< endl;
+
+
+int main() {
+#ifndef _OJ_
+    freopen("/Users/xxy/CLionProjects/DSA/PA_2/input.txt", "r", stdout);
+//    freopen("/Users/xxy/CLionProjects/DSA/PA_2/output.txt", "w", stdin);
+#endif
+    Tik
+
+
+    Tok
+    return 0;
+}

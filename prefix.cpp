895462e7a0d14478ac774ac2b16e4978e8b3f650
xiangxiaoyu <xcd19@mails.tsinghua.edu.cn>
2021-12-04 10:07:02 +0800

    prefix

--- prefix.cpp
+++ prefix.cpp
@@ -6,40 +6,51 @@
 #include <string.h>
 
 using namespace std;
-typedef long l;
-l nxt[20000001] = {0};
-l ct[20000001] = {0};
+typedef int l;
+l nxt[20000001] = {-1};
+//l ct[20000001] = {0};
 int len[20000001] = {0};
 char s[20000001] = "";
 
 
 int main() {
 #ifndef _OJ_
-    freopen("/Users/iMac-2/CLionProjects/PA_3/input.txt", "r", stdin);
+//    freopen("/Users/iMac-2/CLionProjects/PA_3/output.txt", "r", stdin);
+    freopen("/Users/xxy/CLionProjects/DSA/PA_3/input.txt", "r", stdin);
 #endif
     nxt[0] = -1;
-    ct[0] = 1;
     len[0] = 1;
     scanf("%s", s);
-    int length = 0;
+    long long ans = 1;
     for (int i = 1; i < 20000001; ++i) {
-        if (!(s[i]>='a' and s[i]<='z'))break;
+        if (!(s[i] >= 'a' and s[i] <= 'z'))break;
         if (s[nxt[i - 1] + 1] == s[i]) {
             nxt[i] = nxt[i - 1] + 1;
             len[i] = len[nxt[i]] + 1;
-            ct[i] = ct[i - 1] + len[i];
-        } else if (s[i] == s[0]) {
-            nxt[i] = 0;
-            len[i] = 2;
-            ct[i] = ct[i - 1] + len[i];
         } else {
-            nxt[i] = nxt[i - 1];
-            len[i] = 1;
-            ct[i] = ct[i - 1] + len[i];
+            l j = nxt[i - 1];
+            while (j != -1) {
+                if (s[j + 1] == s[i]) {
+                    nxt[i] = j + 1;
+                    len[i] = len[nxt[i]] + 1;
+                    break;
+                }
+                j = nxt[j];
+            }
+            if (j == -1) {
+                if (s[i] == s[0]) {
+                    nxt[i] = 0;
+                    len[i] = 2;
+                } else {
+                    len[i] = 1;
+                    nxt[i] = -1;
+                }
+            }
         }
-        length ++;
+//        printf("i%d, %d %d  %lld\n", i, nxt[i], len[i], ans);
+        ans += (long long) len[i];
     }
-    cout << ct[length] << endl;
+    cout << ans << endl;
 
     return 0;
 }

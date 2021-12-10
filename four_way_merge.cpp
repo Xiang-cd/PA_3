4398e9a4f2cdea5a111415933b5fa66275f047fb
xiangxiaoyu <xcd19@mails.tsinghua.edu.cn>
2021-12-10 17:08:48 +0800

    ok

--- four_way_merge.cpp
+++ four_way_merge.cpp
@@ -11,6 +11,7 @@ int Total = 0;
 #define debug(...) fprintf(stderr, __VA_ARGS__)
 
 int buffer[1000010] = {};
+int buffer_size = 0;
 
 bool compare(int x, int y) {
     int max, min;
@@ -33,25 +34,39 @@ inline void swap(int &a, int &b) {
 }
 
 
-void pbuff(int left_size, int right_size, int n, int *a, int start, int end, int mid) {
-    printf("%d %d  %d\n", start, end, mid);
-    cout << "buffer:";
+void pbuff(int left_size) {
+    debug("buffer:");
     for (int i = 0; i < left_size; ++i) {
-        printf("%d ", buffer[i]);
+        debug("%d ", buffer[i]);
     }
-    cout << endl;
-    for (int i = 0; i < n; ++i) {
-        printf("%d ", a[i]);
-    }
-    cout << endl;
+    debug("\n");
 }
 
-inline void select_number(int *cmp_var, int flag, int *begin, int *a) {
+inline void select_number(int *cmp_var, int flag, int *begin, int *a, int *lens) {
     int index = 0;
     for (int i = 0; i < 4; ++i) {
         if (i == flag)continue;
         cmp_var[index++] = a[begin[i]];
     }
+}
+
+inline void get_into_buffer(int *cmp_var, int min, int unselect_pos, int *begins, int *lens) {
+    int i;
+    for (i = 0; i < 3; ++i) if (min == cmp_var[i])break; //将最小元放入缓冲区
+    buffer[buffer_size++] = cmp_var[i];
+//    pbuff(buffer_size);
+    if (i >= unselect_pos)i++; //确定是哪一路的子序列被放入
+    if (lens[i]) {
+        begins[i]++;
+        lens[i]--;
+    }
+}
+
+inline void change_flag(int *cmpvar, int max, int min, int &flag) {
+    int i;
+    for (i = 0; i < 3; ++i) if (max == cmpvar[i])break;
+    if (i >= flag)i++;
+    flag = i;
 }
 
 
@@ -81,24 +96,77 @@ void four_way_sort(int start, int end, int *a, int n) {
     int mid1 = start + (end - start) / 4;
     int mid2 = start + (end - start) / 2;
     int mid3 = start + 3 * (end - start) / 4;
+//    debug("%d %d %d %d %d\n", start, mid1, mid2, mid3, end);
     four_way_sort(start, mid1, a, n);
     four_way_sort(mid1, mid2, a, n);
     four_way_sort(mid2, mid3, a, n);
     four_way_sort(mid3, end, a, n);
     // 递归结束后进行归并
+    buffer_size = 0;
     int unselect_pos = 3; //假设开始比较时不选择第四路
     int max, min;
     int cmp_var[3] = {0};
     int begins[4] = {start, mid1, mid2, mid3};
     int lens[4] = {mid1 - start, mid2 - mid1, mid3 - mid2, end - mid3};
-    while (lens[0] > 0 or lens[1] > 0 or lens[2] > 0 or lens[3] > 0) {
-        select_number(cmp_var, unselect_pos, begins, a);
+    select_number(cmp_var, unselect_pos, begins, a, lens);
+    compare(cmp_var[0], cmp_var[1], cmp_var[2], &max, &min);
+    change_flag(cmp_var, max, min, unselect_pos);
+    while (lens[0] and lens[1] and lens[2] and lens[3]) { //确保四路存在
+//        debug("%d %d %d %d  %d\n", lens[0], lens[1], lens[2], lens[3], unselect_pos);
+        select_number(cmp_var, unselect_pos, begins, a, lens);
         compare(cmp_var[0], cmp_var[1], cmp_var[2], &max, &min);
-
-
+        get_into_buffer(cmp_var, min, unselect_pos, begins, lens);
+        change_flag(cmp_var, max, min, unselect_pos);//根据最大值确定下一个不选择的路
+    }
+    int count = 0;
+    for (int i = 0; i < 4; ++i) if (lens[i])count++;
+    if (count == 3) {// 三路归并
+        for (unselect_pos = 0; unselect_pos < 4; unselect_pos++)if (!lens[unselect_pos]) break;
+        while (1) {
+            int num = 0;
+            for (int i = 0; i < 4; ++i) if (!lens[i])num++;
+            if (num > 1)break;
+//            debug("%d %d %d %d  %d\n", lens[0], lens[1], lens[2], lens[3], unselect_pos);
+            select_number(cmp_var, unselect_pos, begins, a, lens);
+            compare(cmp_var[0], cmp_var[1], cmp_var[2], &max, &min);
+            get_into_buffer(cmp_var, min, unselect_pos, begins, lens);
+        }
+    }
+    count = 0;
+    for (int i = 0; i < 4; ++i) if (lens[i])count++;
+    if (count == 2) { // 二路归并
+        int rot[2];
+        int index = 0;
+        for (int i = 0; i < 4; ++i)if (lens[i])rot[index++] = i;
+        while (lens[rot[0]] and lens[rot[1]]) {
+//            debug("%d %d %d %d  %d\n", lens[0], lens[1], lens[2], lens[3], unselect_pos);
+            if (compare(a[begins[rot[0]]], a[begins[rot[1]]])) {
+                buffer[buffer_size++] = a[begins[rot[0]]++];
+                lens[rot[0]]--;
+            } else {
+                buffer[buffer_size++] = a[begins[rot[1]]++];
+                lens[rot[1]]--;
+            }
+        }
     }
 
+    count = 0;
+    for (int i = 0; i < 4; ++i) if (lens[i])count++;
+    if (count <= 1) { //
+//        debug("%d %d %d %d  %d\n", lens[0], lens[1], lens[2], lens[3], unselect_pos);
+        int posi;
+        for (posi = 0; posi < 4; posi++) if (lens[posi])break;
+//        debug("%d \n",posi);
+        if (posi != 4) {
+            for (int i = 0; i < lens[posi]; ++i) {
+                buffer[buffer_size++] = a[begins[posi]++];
+            }
+        }
+//        pbuff(buffer_size);
+    }
 
+    if (buffer_size != end - start)exit(122);
+    memcpy(a + start, buffer, sizeof(int) * buffer_size);
 }
 
 
